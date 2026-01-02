638974c54a0611723ab72752d45d1ffb91e2f961
ewigl <ewiglichtl@gmail.com>
2026-01-02 19:16:31 +0800

    refactor: fix tagline typo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Licht\'s Zone',
-  tagline: 'Living, Leaning and Enjoying.',
+  tagline: 'Living, Learning and Enjoying.',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
